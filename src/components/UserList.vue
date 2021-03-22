6237016354247b10ca35cfe64c92e8a22dfc907a
ktapia576 <ktapia576@hotmail.com>
2021-03-21 21:43:04 -0400

    Add random user fetching and displaying

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,20 +1,37 @@
 <template>
-
+  <ul class="list-group">
+    <li 
+      class="list-group-item list-group-item-secondary"
+      v-for="user in results"
+      :key="user.id.value"
+    >
+      {{ user.name.first }}
+    </li>
+  </ul>
 </template>
 
 <script>
 import axios from 'axios'
 
 export default {
-  name: 'UserList'
+  name: 'UserList',
+  data() {
+    return {
+      results: [
+        {name: {first: "default"} , id: {value: 127831 }}
+      ]
+    }
+  },
+  mounted() {
+    // Get random users
+    axios.get("https://randomuser.me/api/?results=5")
+      .then(res => {
+        console.log(res)
+        this.results = res.data.results
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  }
 }
-
-axios.get("https://randomuser.me/api/?results=5")
-  .then(res => {
-    console.log(res)
-  })
-  .catch(err => {
-    console.log(err)
-  });
-
 </script>
