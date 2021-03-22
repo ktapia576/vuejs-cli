cc4367312e1054efde13835a3945760ede29937c
ktapia576 <ktapia576@hotmail.com>
2021-03-21 21:03:28 -0400

    Remove axios call in helloworld component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,23 +5,12 @@
 </template>
 
 <script>
-import axios from 'axios'
-
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
   }
 }
-
-axios.get("https://randomuser.me/api/?results=5")
-  .then(res => {
-    console.log(res)
-  })
-  .catch(err => {
-    console.log(err)
-  });
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
