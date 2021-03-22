bd491b64a0b2f35f502a6ea79a71996ac721cc4e
ktapia576 <ktapia576@hotmail.com>
2021-03-21 21:02:29 -0400

    Add userList component

---
+++ src/components/UserList.vue
@@ -0,0 +1,41 @@
+<template>
+
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+
+axios.get("https://randomuser.me/api/?results=5")
+  .then(res => {
+    console.log(res)
+  })
+  .catch(err => {
+    console.log(err)
+  });
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
