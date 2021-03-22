a248e89be71684becf0fd0a2e399de878c12385a
ktapia576 <ktapia576@hotmail.com>
2021-03-21 21:42:33 -0400

    import UserList component and add to template

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <UserList />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import UserList from './components/UserList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    UserList
   }
 }
 </script>
