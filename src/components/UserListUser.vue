65bc57746a051adde9056b53da9b01b53e148360
ktapia576 <ktapia576@hotmail.com>
2021-03-21 23:04:26 -0400

    Have component with users prop and li elements to display suers

--- src/components/UserListUser.vue
+++ src/components/UserListUser.vue
@@ -1,9 +1,18 @@
 <template>
-
+    <li 
+        class="list-group-item list-group-item-success"
+        v-for="user in users"
+        :key="user.id.value"
+    >
+        {{ user.name.first }} {{ user.name.last }} | Email: {{ user.email }}
+    </li>
 </template>
 
 <script>
 export default {
-  name: 'UserListUser'
+    name: 'UserListUser',
+    props: {
+        users: Array,
+    }
 }
 </script>
